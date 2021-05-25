9ed25459e1d5d853a1c46eacfabdc439f72ea39d
wjddpdnjs456 <79553922+wjddpdnjs456@users.noreply.github.com>
2021-05-25 13:18:19 +0900

    2021-05-25

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
 
-function App(){
+function formatDate(date){
+  return date.toLocaleDateString();
+}//이건함수
+
+function Comment(props){
   return(
-    <div>
-      <Welcome name="jung" />
-      <Welcome name="ye" />
-      <Welcome name="won" />  
+    <div className="comment">
+        <div className="user-info">
+          <img className="avatar" src={props.author.avatarUrl} alt={props.author.name} />
+          <div className="user-name">{props.author.name}</div>
+        </div>
+        <div className="comment-text">{props.text}</div>
+        <div className="comment-date">{formatDate(props.date)}</div>
     </div>
-  )
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+  );
+}//이건 컴퍼넌트
+
+const comment ={
+  date: new Date(),
+  text: "열심히 할수록 기회는 따른다.",
+  author: {
+    name: "yewon",
+    avatarUrl:"http://yewon5184.dothome.co.kr/portfolio/assets/imges/aboutImg01.jpg"
+  },
+};
+
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author= {comment.author}/>, document.getElementById("root"));
 //render는 출렷한다는 뜻
 
 
