1a3234099b4471077e2af6b6c4caa2065ecfe6ea
wjddpdnjs456 <79553922+wjddpdnjs456@users.noreply.github.com>
2021-05-26 10:14:18 +0900

    2021-05-26

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 function formatDate(date){
   return date.toLocaleDateString();
-}//이건함수
-
+}
+function Avatar(props){
+    return(
+      <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name}/>
+    )
+  }
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+      <Avatar user={props.user}/>
+      <div>{props.user.name}</div>
+      <div>UserInfo</div>
+    </div>
+  )
+}
 function Comment(props){
   return(
-    <div className="comment">
-        <div className="user-info">
-          <img className="avatar" src={props.author.avatarUrl} alt={props.author.name} />
-          <div className="user-name">{props.author.name}</div>
-        </div>
-        <div className="comment-text">{props.text}</div>
-        <div className="comment-date">{formatDate(props.date)}</div>
+    <div className="Comment">
+      <UserInfo user={props.author}/>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-text">{formatDate(props.date)}</div>
     </div>
-  );
-}//이건 컴퍼넌트
-
+  )
+}
 const comment ={
   date: new Date(),
   text: "열심히 할수록 기회는 따른다.",
@@ -27,13 +35,4 @@ const comment ={
     avatarUrl:"http://yewon5184.dothome.co.kr/portfolio/assets/imges/aboutImg01.jpg"
   },
 };
-
-ReactDOM.render(
-  <Comment date={comment.date} text={comment.text} author= {comment.author}/>, document.getElementById("root"));
-//render는 출렷한다는 뜻
-
-
-
-
-
-
+ReactDOM.render(<Comment date={comment.date} text={comment.text} author={comment.author}/>, document.getElementById("root"));
